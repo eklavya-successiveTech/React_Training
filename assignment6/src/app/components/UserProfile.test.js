5ab91b729a80fab0b143627ffae3a75a143efae3
Eklavya Sanmotra <eklavya.sanmotra@eklavyavrs-28.successivetech.com>
2025-07-02 10:07:38 +0530

    feature/assignment6

--- assignment6/src/app/components/UserProfile.test.js
+++ assignment6/src/app/components/UserProfile.test.js
@@ -2,18 +2,18 @@ import { render, screen } from '@testing-library/react';
 import UserProfile from './UserProfile';
 
 describe('UserProfile Component', () => {  const mockUser = {
-    name: 'Aarushi Mahajan',
-    email: 'aarushi.m@example.com',
+    name: 'eklavya sanmotra',
+    email: 'eklavya@example.com',
     phone: '123-456-7890',
   };
   test('renders user name, email, and phone number from props', () => {
         render(<UserProfile user={mockUser} />);
 
 
-    const nameElement = screen.getByRole('heading', { name: /aarushi mahajan/i });
+    const nameElement = screen.getByRole('heading', { name: /eklavya sanmotra/i });
     expect(nameElement).toBeInTheDocument();
 
-    const emailElement = screen.getByText(/aarushi.m@example.com/i);
+    const emailElement = screen.getByText(/eklavya@example.com/i);
     const phoneElement = screen.getByText(/123-456-7890/i);
 
     expect(emailElement).toBeInTheDocument();
